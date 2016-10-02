3620189f74511ec8315d64447a1d3a3ec69ae408
gil lawson <gewl@users.noreply.github.com>
2016-10-02 18:46:39 -0400

    Added rough versions of login, logout, and filechange reception. Also began to format JSON used for electron's local storage.

--- app/utils/file-watch.js
+++ app/utils/file-watch.js
@@ -7,10 +7,28 @@ import Promise from 'bluebird';
 import { logParser, diffParser } from './git-command-parsers.js';
 //for information about these functions, see reference at the top of './git-command-parsers.js'
 
+let channels;
 let repoPaths;
+let githubName;
 //TOCHANGE: once we've integrated local storage of a user's channels, use the logic below
 //
-//storage.get('repos', ( err, data ) => {
+//storage.get('githubName', ( err, data ) => {
+//	if (err) {
+//		console.error(err)
+//		return;
+//	}
+//
+//	githubName = data
+//})
+//storage.get('channels', ( err, data ) => {
+//	if (err) {
+//		console.error(err)
+//		return;
+//	}
+//
+//	channels = data
+//})
+//storage.get('repoPaths', ( err, data ) => {
 //	if (err) {
 //		console.error(err)
 //		return;
@@ -18,24 +36,44 @@ let repoPaths;
 //
 //	repoPaths = data
 //})
+//n.b.: This assumes that repos and channels will be stored in separate arrays, where
+//the channel at a given index value is linked to the path at the same index value in
+//repoPaths
 
 //TOCHANGE: for the time being, using this to watch files in this directory:
+channels = ['rgit-testing']
 repoPaths = ['**/*']
 
-
 //Iterate through the user's repos and set up a watcher for each one.
-repoPaths.forEach(path => {
+channels.forEach((channel, index) => {
+	let repoPath = repoPaths[index]
 
-	gaze(path, {
+	gaze(repoPath, {
 		//Don't watch npm packages in a repo—it's prohibitively expensive and they shouldn't be
 		//doing anything in there anyway.
 		ignore: ['node_modules/**']
 	}, function(err, watcher) {
-		//'All' will fire the callback if any files are changed, added, or deleted.
-		this.on('all', function() {
-			let payload = {}
+		//TOCHANGE: To avoid giving other users the emitting user's file structure,
+		//going to need to isolate filepath relative to repo directory.
+		//
+		//let pathRegex = new RegExp( repoPath + '([\\s\\S]*)', 'g')
 
-			git(path)
+		//'All' will fire the callback if any files are changed, added, or deleted.
+		this.on('all', function(event, filepath) {
+			let payload = { event, channel, githubName }
+			//Check to see if the gaze trigger was a file being added.
+			//This can cut down on some logic downstream, because
+			//other people couldn't be watching a file that was just created.
+			let fileAdded = false
+
+			//TOCHANGE: once repo paths are being stored, add this bit
+			//let revisedPath = pathRegex.exec(filepath)
+			payload.filepath = filepath
+			
+			if (event === "added") fileAdded = true
+			payload.fileAdded = fileAdded
+
+			git(repoPath)
 				.branch( (err, result) => {
 					if (err) {
 						console.error(err)
@@ -88,8 +126,35 @@ repoPaths.forEach(path => {
 				.then(() => {
 					socket.emit('fileChanges', payload)
 				})
-
 		})
 	})
+})
 
+//TOCHANGE: Upon logging in, the client should send login name to the server—
+//this will be used to add the client to a number of rooms, which will be
+//used for broadcasting filechange events.
+//
+//socket.emit('login', { githubId: githubId })
+
+//Additionally, on window close (in main electron process?), should emit:
+//
+//socket.emit('logout', { githubId: githubId })
+
+//Logic for handling incoming socket events—this will probably be moved to
+//a parent react component.
+
+socket.on('fileChanges', payload => {
+	let channels
+	storage.get('channels', (err, data) => {
+		if (err) {
+			console.error(err)
+			return
+		}
+
+		channels = data
+	})
+
+	if (channels.includes(payload.channel)) {
+		new Notification(payload.githubName + ' is editing ' + payload.filepath + ' in ' + payload.branch.current + '.')
+	}
 })
