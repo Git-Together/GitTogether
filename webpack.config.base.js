386c1152bd1e0a2ff30ef2a0d434663f38b3faa7
MiladNazeri <milad@miladnazeri.com>
2016-10-10 18:42:37 -0400

    uiRedesign midway

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -10,8 +10,8 @@ export default validate({
     loaders: [{
       test: /\.jsx?$/,
       loader: 'babel-loader',
-		exclude: /node_modules/,
-		query: { presets: ['es2015', 'react', 'stage-0'] }
+    exclude: /node_modules/,
+    query: { presets: ['es2015', 'react', 'stage-0'] }
     }, {
       test: /\.json$/,
       loader: 'json-loader'
@@ -35,7 +35,7 @@ export default validate({
   plugins: [],
 
   externals: [
-	  'bootstrap',
-	  'dotenv'
+    'bootstrap',
+    'dotenv'
   ]
 });
--- webpack.config.development.js
+++ webpack.config.development.js
@@ -47,8 +47,10 @@ export default validate(merge(baseConfig, {
         test: /^((?!\.global).)*\.scss$/,
         loaders: [
           'style-loader',
-          'css-loader?modules&sourceMap&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
-          'sass?modules&sourceMap&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'
+          // 'css-loader?modules&sourceMap&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
+          // 'sass?modules&sourceMap&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'
+          'css-loader?',
+          'sass'
         ]
       }
     ]
