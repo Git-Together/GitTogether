33861ab2eabbed796cb15f20dfe6deacbf669dde
MiladNazeri <milad@miladnazeri.com>
2016-10-10 22:12:23 -0400

    base uiRedesign Done

--- app/components/Home.js
+++ app/components/Home.js
@@ -16,25 +16,29 @@ export default class Home extends Component {
 
   render() {
 		return (
-      <div id='Container'> {/* +Container */}
+      <div id='Container'>
 
-        <div id="Header">  {/* +Header */}
+        <div id="Header">
 
-          <div id="Header-Logo">  {/* +Header-Logo */}
+          <div id="Header-Logo">
 
-            <div id="Header-Logo-Image">  {/* +Header-Logo-Image */}
+            <div id="Header-Logo-Image">
               <span id="Header-Logo-Image-Git">Git</span>
               <span id="Header-Logo-Image-Together"><em>Together</em></span>
             </div> {/* -Header-Logo-Image */}
 
           </div> {/* -Header-Logo */}
-          <div id="Header-Nav">  {/* +Header-Nav */}
+          <div id="Header-Nav">
 
-            <div id="Header-Nav-Text">  {/* +Header-Nav-Text */}
-              Header Nav Text
+            <div id="Header-Nav-Text">
+              <span id="Header-Nav-Text-Welcome">Welcome</span>
+              <span id="Header-Nav-Text-UN"><em>Milten Kingil</em></span>
             </div> {/* -Headder-Nav-Text */}
-            <div id="Header-Nav-IconBar">  {/* +Header-Nav-IconBar */}
-              Header Nav IconBar
+            <div id="Header-Nav-IconBar">
+              {
+                ['glyphicon glyphicon-eye-open', 'glyphicon glyphicon-cloud-upload', 'glyphicon glyphicon-user', 'glyphicon glyphicon-tasks', 'glyphicon glyphicon-file', 'glyphicon glyphicon-comment']
+                .map(e => { return <div key={e} className={`Header-Nav-IconBar-Icons ${e}`}></div>})
+              }
             </div> {/* -Header-Nav-IconBar */}
 
           </div> {/* -Header-Nav */}
@@ -43,34 +47,34 @@ export default class Home extends Component {
 
         <div id="Body">  {/* +Body */}
 
-          <div id="Body-RepoTeam">  {/* +Body-RepoTeam */}
+          <div id="Body-RepoTeam">
 
-            <div id="Body-RepoTeam-Repo">  {/* +Body-RepoTeam-Repo */}
+            <div id="Body-RepoTeam-Repo">
               Body-RepoTeam-Repo
             </div> {/* -Body-RepoTeam-Repo */}
-            <div id="Body-RepoTeam-Team">  {/* +Body-RepoTeam-Team */}
+            <div id="Body-RepoTeam-Team">
               Body-RepoTeam-Team
             </div> {/* -Body-RepoTeam-Team */}
 
           </div> {/* -Body-RepoTeam */}
-          <div id="Body-Content">  {/* +Body-Content  */}
+          <div id="Body-Content">
 
-            <div id="Body-Content-Watch">  {/* +Body-Content-Watch */}
+            <div id="Body-Content-Watch">
               Body-Content-Watch
             </div> {/* -Body-Content-Watch */}
-            <div id="Body-Content-Repo">  {/* +Body-Content-Repo */}
+            <div id="Body-Content-Repo">
               Body-Content-Repo
             </div> {/* -Body-Content-Repo */}
-            <div id="Body-Content-Member">  {/* +Body-Content-Member */}
+            <div id="Body-Content-Member">
               Body-Content-Member
             </div> {/* -Body-Content-Member */}
-            <div id="Body-Content-Channel">  {/* +Body-Content-Channel */}
+            <div id="Body-Content-Channel">
               Body-Content-Channel
             </div> {/* -Body-Content-Channel */}
-            <div id="Body-Content-File">  {/* +Body-Content-File */}
+            <div id="Body-Content-File">
               Body-Content-File
             </div> {/* -Body-Content-File */}
-            <div id="Body-Content-Chat">  {/* +Body-Content-Chat */}
+            <div id="Body-Content-Chat">
               Body-Content-Chat
             </div> {/* -Body-Content-Chat */}
 
