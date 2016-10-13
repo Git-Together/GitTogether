5180f33d91ed9ce6bf2b0067b00ee8b1fd3cb56b
Ten <tenloh@gmail.com>
2016-10-13 14:44:55 -0400

    for kin

--- app/components/_shared/List/ListItem-component.js
+++ app/components/_shared/List/ListItem-component.js
@@ -14,7 +14,16 @@ class List extends Component {
   };
 
   render() {
-    let item = this.props.item.name ? this.props.item.name : this.props.item;
+    let item;
+    if(typeof this.props.item === "string"){
+      item = this.props.item;
+    } else {
+      if(this.props.item.name){
+        item = this.props.item.name;
+      } else if (this.props.item.path){
+        item = this.props.item.path
+      }
+    }
     return (
       <div className="ListItem" onClick={this.props.changeSelected.bind(null, item)}>
 
