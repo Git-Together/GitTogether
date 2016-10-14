6780f3e2e13cb7723f30430e18bb8a7875e02fc6
Kin Tsang <kinsanity@gmail.com>
2016-10-14 17:48:02 -0400

    WIP watch

--- app/utils/incoming-sockets.js
+++ app/utils/incoming-sockets.js
@@ -5,6 +5,8 @@ import * as ChannelActions from '../actions/channel-actions.js'
 import * as AuthActions from '../actions/auth-actions.js'
 import * as FileActions from '../actions/file-actions.js'
 import * as RepoActions from '../actions/repo-actions.js'
+import axios from 'axios';
+
 const storage = Promise.promisifyAll(require('electron-json-storage'))
 
 let socket = null
@@ -12,23 +14,46 @@ export function instantiateSockets (state, dispatch) {
 	socket = io.connect(process.env.SOCKET_URL)
 	dispatch(AuthActions.socketsStarted())
 	let currentUser = state.auth.currentUser
+  let userId = state.auth.id
 	socket.emit('passLogin', currentUser)
 
 	socket.on('fileChanges', payload => {
 		let channels
+    let watchFileList = [];
+
+    // //obtain a list of files currently watching from server
+    // axios.get(process.env.SERVER_URL + `/api/files/?userId=${userId}`)
+    // .then((watchList) => {
+    //   console.log("watchList: ", watchList.data)
+    //   watchFileList = watchList.data
+    // })
+
 		dispatch(FileActions.addMessageToFilePanel({
 			label: payload.channel,
 			test: payload.filepath
 		}))
-		if (payload.username != currentUser) {
+		// if (payload.username != currentUser) {
 			storage.getAsync('channels')
 				.then(cachedChannels => {
+          console.log("cachedChannels from incoming-sockets.js: ",cachedChannels)
+
 					let channels = Object.keys(cachedChannels[currentUser])
-					if (channels.includes(payload.channel)) {
-						new Notification(payload.username + ' just ' + payload.event + " " + payload.filepath + ' in ' + payload.branch.current + '.', { silent: 'true' })
-					}
+
+          watchFileList.forEach((e, index) => {
+            if(e.fileName === payload.filepath && e.repoId === payload.branch.current){
+              new Notification(payload.username + ' just ' + payload.event + " " + payload.filepath + ' in ' + payload.branch.current + '.', { silent: 'true' })
+            }
+
+          })
+
+					// if (channels.includes(payload.channel)) {
+					// 	new Notification(payload.username + ' just ' + payload.event + " " + payload.filepath + ' in ' + payload.branch.current + '.', { silent: 'true' })
+					// }
+
+
 				})
-		}
+		// } //if statement closes
+
 	})
 
 	socket.on('reloadTeam', channelName => {
